9f27f443648a31403a87794b98cd3939db98596a
Sachithra Pinnaduwa <sachithrapinnaduwa@gmail.com>
2026-02-07 00:21:35 +0530

    Fix default port assignment in server configuration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 
-const PORT = process.env.PORT || 5000;  //logicl or operator
+const PORT = process.env.PORT;  //logicl or operator
 
 app.use(cors());
 app.use(bodyParser.json()); //get json data using body parser
